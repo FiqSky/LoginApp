99e1a1659e6f7c88092630c809923ef8cc2b4823
FiqSky <fiqihfm05@gmail.com>
2021-01-10 10:33:30 +0700

    Initial commit

---
+++ app/src/main/java/com/fiqsky/loginapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.fiqsky.loginapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val timeOut : Long = 2000 //2 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        Handler().postDelayed({
+            if (user != null){
+                val dashboardIntent = Intent(this, DashboardActivity::class.java)
+                startActivity(dashboardIntent)
+                finish()
+            }else{
+                val signInIntent = Intent(this, SignInActivity::class.java)
+                startActivity(signInIntent)
+                finish()
+            }
+        }, timeOut)
+    }
+}
